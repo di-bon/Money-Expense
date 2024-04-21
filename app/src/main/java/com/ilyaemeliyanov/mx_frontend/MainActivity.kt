e85bac4d117bde8c72eac3a449dffe5dc2997b84
IlyaEmeliyanov <ilya.emeliyanov20@gmail.com>
2024-04-21 21:56:03 +0200

    Add bottom NavigationBar

--- app/src/main/java/com/ilyaemeliyanov/mx_frontend/MainActivity.kt
+++ app/src/main/java/com/ilyaemeliyanov/mx_frontend/MainActivity.kt
@@ -3,26 +3,41 @@ package com.ilyaemeliyanov.mx_frontend
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ilyaemeliyanov.mx_frontend.ui.theme.MxfrontendTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.ilyaemeliyanov.mx_frontend.ui.theme.MXColors
+import com.ilyaemeliyanov.mx_frontend.ui.theme.MXIcons
+import com.ilyaemeliyanov.mx_frontend.ui.theme.MXTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MxfrontendTheme {
+            MXTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MXColors.Default.bgColor
                 ) {
-                    Greeting("Android")
+                    MXScreen()
                 }
             }
         }
@@ -30,17 +45,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MXScreen() {
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar()
+        }
+    ) { padding ->
+        Column(modifier=Modifier.padding(padding)) {
+            Text("Dashboard")
+        }
+    }
 }
+
+// Bottom navbar
+data class NavbarItem(
+    val route: String,
+    val label: String,
+    val icon: ImageVector
+)
+@Composable
+fun BottomNavigationBar() {
+    NavigationBar(
+        containerColor = MXColors.Navbar.navbarBgColor,
+        contentColor = Color.White
+    ) {
+
+        val navBarItems = listOf(
+            NavbarItem("/dashboard", "Dashboard", ImageVector.vectorResource(MXIcons.Default.dashboardIconId)),
+            NavbarItem("/wallets", "Wallets", ImageVector.vectorResource(MXIcons.Default.walletsIconId)),
+            NavbarItem("/transactions", "Transactions", ImageVector.vectorResource(MXIcons.Default.transactionsIconId)),
+            NavbarItem("/settings", "Settings", ImageVector.vectorResource(MXIcons.Default.settingsIconId))
+        )
+        var selectedIndex by remember { mutableIntStateOf(0) }
+
+        navBarItems.forEachIndexed { index, item ->
+            NavigationBarItem (
+                selected = selectedIndex == index,
+                onClick = {
+                      selectedIndex = index
+                },
+                icon = { Icon(item.icon, tint=(if (selectedIndex == index) MXColors.Navbar.iconSelectedColor else MXColors.Navbar.iconDefaultColor), contentDescription = null, modifier=Modifier.padding(10.dp)) },
+                label = { Text(item.label, color=MXColors.Default.secondaryColor) }
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MxfrontendTheme {
-        Greeting("Android")
+    MXTheme {
+        BottomNavigationBar()
     }
 }
---
+++ app/src/main/java/com/ilyaemeliyanov/mx_frontend/ui/theme/MXIcons.kt
@@ -0,0 +1,12 @@
+package com.ilyaemeliyanov.mx_frontend.ui.theme
+
+import com.ilyaemeliyanov.mx_frontend.R
+
+object MXIcons {
+    object Default {
+        val dashboardIconId = R.drawable.dashboard_icon
+        val walletsIconId = R.drawable.wallets_icon
+        val transactionsIconId = R.drawable.transactions_icon
+        val settingsIconId = R.drawable.settings_icon
+    }
+}
