7da1047e548dd644de0c0cb921f49d2adde9f252
di-bon <dibon.francesco03@gmail.com>
2024-07-24 13:02:16 +0200

    MxInput created and RegisterScreen updated

---
+++ app/src/main/java/com/ilyaemeliyanov/mx_frontend/ui/composables/MxInput.kt
@@ -0,0 +1,53 @@
+package com.ilyaemeliyanov.mx_frontend.ui.composables
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ilyaemeliyanov.mx_frontend.ui.theme.MXTheme
+
+@Composable
+fun MxInput(
+    modifier: Modifier = Modifier,
+    titleText: String,
+    labelText: String
+) {
+    var text by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        Text(text = titleText)
+//        Spacer(modifier = Modifier.height(4.dp))
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(text = labelText) },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MxInputPreview() {
+    MXTheme {
+        MxInput(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            titleText = "First name",
+            labelText = "Enter your first name..."
+        )
+    }
+}
--- app/src/main/java/com/ilyaemeliyanov/mx_frontend/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/ilyaemeliyanov/mx_frontend/ui/screens/RegisterScreen.kt
@@ -1,22 +1,84 @@
 package com.ilyaemeliyanov.mx_frontend.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ilyaemeliyanov.mx_frontend.ui.composables.MxInput
+import com.ilyaemeliyanov.mx_frontend.ui.composables.MxRectangularButton
 import com.ilyaemeliyanov.mx_frontend.ui.theme.MXTheme
 
 @Composable
-fun RegisterScreen(modifier: Modifier = Modifier) {
-    Text(text = "Register screen")
+fun RegisterScreen(
+    onSignUpClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "Sign up", style = MaterialTheme.typography.displayLarge)
+        Column {
+            MxInput(
+                titleText = "First name",
+                labelText = "Enter your first name...",
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            MxInput(
+                titleText = "Last name",
+                labelText = "Enter your last name...",
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            MxInput(
+                titleText = "Email",
+                labelText = "Enter your email...",
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            MxInput(
+                titleText = "Password",
+                labelText = "Enter your password...",
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            MxInput(
+                titleText = "Confirm password",
+                labelText = "Enter your password...",
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+        }
+
+        MxRectangularButton(
+            onClick = onSignUpClick,
+            containerColor = Color.Black,
+            contentColor = Color.White,
+            modifier = Modifier
+                .height(60.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Sign up",
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun RegisterScreenPreview() {
     MXTheme {
-        RegisterScreen(modifier = Modifier.padding(16.dp))
+        RegisterScreen({}, modifier = Modifier
+            .padding(32.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+        )
     }
 }
